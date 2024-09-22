977b63f359b6e81485ad732e49fb660a773e24d9
Edwin Cely <edwincely6@gmail.com>
2024-09-22 09:26:36 -0600

    orderController getORderById

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,10 +1,26 @@
 import Order from "../models/Order.js";
 
-export const getAllOrders = async (req, res) =>{
-try {
-    const orders = await Order.find({ deleteAt: null});
+export const getAllOrders = async (req, res) => {
+  try {
+    const orders = await Order.find({ deleteAt: null });
     return res.json(200).json(orders);
-}catch (error) {
-    return res.status(500).json({message: `Error al obtener los pedidos`, error});
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: `Error al buscar todos los pedidos`, error });
+  }
 };
+
+export const getORderById = async (req, res) => {
+  try {
+    const orderId = await Order.findById(req.params.id);
+    if (!orderId || orderId.deleteAt) {
+      return res.status(404).json({ message: `Pedido no encontrado` });
+    }
+    return res.status(200).json(orderId);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Error al buscar el pedido", error });
+  }
 };
