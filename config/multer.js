9cce981f0dff5437bb084f558a70fae72342d514
Edwin Cely <edwincely6@gmail.com>
2024-09-23 19:44:19 -0600

    prettier multer.js

--- config/multer.js
+++ config/multer.js
@@ -1,17 +1,16 @@
 import multer from "multer";
 import path from "path";
 
-// Set up Multer storage
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "uploads/"); // Directory where files will be saved
+        cb(null, "uploads/"); 
     },
     filename: (req, file, cb) => {
-        cb(null, Date.now() + "-" + file.originalname); // Unique file name
+        cb(null, Date.now() + "-" + file.originalname); 
     },
 });
 
-// Create the Multer instance
+
 const upload = multer({ storage });
 
 export default upload;
